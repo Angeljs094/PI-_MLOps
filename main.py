fc63bc0f4e980768f2ef1a631073032d9224fa67
angeljs094 <angeljs094@gmail.com>
2024-02-05 23:20:34 -0500

    Mi tercer commit

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from typing import List, Optional
 from fastapi.exceptions import HTTPException
 import pandas as pd
 import numpy as np
+from sklearn.metrics.pairwise        import cosine_similarity
+from sklearn.metrics.pairwise        import linear_kernel
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 app=FastAPI(debug=True)
@@ -10,13 +13,13 @@ app=FastAPI(debug=True)
 df = pd.read_csv('data_final.csv')
 
 @app.get("/")
-def Angel_Jaramillo_Data_Analystic():
+def Proyecto_de_Angel_Jaramillo():
     return "Hello World My name is Angel "
 
 
     # Ejercicio 1
 
-@app.get("/developer-stats/")
+@app.get("/Developer_Stats/")
 def get_developer_stats(developer: str) -> List[dict]:
     # Filtrar solo juegos del desarrollador proporcionado
     df_selected_developer = df[df['developer'].str.lower() == developer.lower()]
@@ -47,7 +50,7 @@ def get_developer_stats(developer: str) -> List[dict]:
 
 
         # Ejercicio 2 
-@app.get('/userdata/')
+@app.get('/User_Data/')
 def userdata(User_id: str) -> dict:
     # Filtramos el DataFrame para obtener solo las filas asociadas al usuario especificado
     user_df = df[df['user_id'] == User_id]
@@ -71,7 +74,7 @@ def userdata(User_id: str) -> dict:
 
 
         # Ejercicio 3
-@app.get('/UserForGenre/')
+@app.get('/User_For_Genre/')
 def UserForGenre(genero: str) -> dict:
     # Convertimos la primera letra del género a mayúscula para asegurarnos de que coincida con los datos
     genero = genero.capitalize()
@@ -92,7 +95,7 @@ def UserForGenre(genero: str) -> dict:
 
 
     # Ejercicio 4
-@app.get('/Best_developer/')
+@app.get('/Best_Developer/')
 def best_developer_year(year: int) -> list[dict]:
     # Filtra el DataFrame para el año dado y con análisis de sentimiento positivo
     df_filtrado = df[(df['year'] == year) & (df['sentiment_analysis'] == 2)]
@@ -110,7 +113,7 @@ def best_developer_year(year: int) -> list[dict]:
 
 
         # Ejercicio 5
-@app.get('/sentiment_analysis/')
+@app.get('/Sentiment_Analysis/')
 def developer_reviews_analysis(desarrollador: str) -> dict:
     # Filtrar el DataFrame por el desarrollador proporcionado
     df_selected_developer = df[df['developer'].str.lower() == desarrollador.lower()]
@@ -123,3 +126,45 @@ def developer_reviews_analysis(desarrollador: str) -> dict:
     result = {desarrollador: {'Positive': positive_count, 'Negative': negative_count}}
 
     return result
+
+muestra = df.head(4000)
+tfidf = TfidfVectorizer(stop_words='english')
+muestra=muestra.fillna("")
+
+tdfid_matrix = tfidf.fit_transform(muestra['review'])
+cosine_similarity = linear_kernel( tdfid_matrix, tdfid_matrix)
+
+@app.get('/recomendacion_id/{id_producto}')
+def recomendacion(id_producto: int):
+    if id_producto not in muestra['steam_id'].values:
+        return {'mensaje': 'No existe el id del juego.'}
+    
+    # Obtener géneros del juego con el id_producto
+    generos = muestra.columns[2:17]  # Obtener los nombres de las columnas de género
+    
+    # Filtrar el dataframe para incluir juegos con géneros coincidentes pero con títulos diferentes
+    filtered_df = muestra[(muestra[generos] == 1).any(axis=1) & (muestra['steam_id'] != id_producto)]
+    
+    # Calcular similitud del coseno
+    tdfid_matrix_filtered = tfidf.transform(filtered_df['review'])
+    cosine_similarity_filtered = linear_kernel(tdfid_matrix_filtered, tdfid_matrix_filtered)
+    
+    idx = muestra[muestra['steam_id'] == id_producto].index[0]
+    sim_cosine = list(enumerate(cosine_similarity_filtered[idx]))
+    sim_scores = sorted(sim_cosine, key=lambda x: x[1], reverse=True)
+    sim_ind = [i for i, _ in sim_scores[1:6]]
+    sim_juegos = filtered_df['title'].iloc[sim_ind].values.tolist()
+    
+    return {'juegos recomendados': list(sim_juegos)}
+
+@app.get('/recomendacion_juego/{id_juego}')
+def recomendacion_juego(id_juego: int):
+    if id_juego not in muestra['id'].values:
+        return {'mensaje': 'No existe el id del juego.'}
+    titulo = muestra.loc[muestra['id'] == id_juego, 'title'].iloc[0]
+    idx = muestra[muestra['title'] == titulo].index[0]
+    sim_cosine = list(enumerate(cosine_similarity[idx]))
+    sim_scores = sorted(sim_cosine, key=lambda x: x[1], reverse=True)
+    sim_ind = [i for i, _ in sim_scores[1:6]]
+    sim_juegos = muestra['title'].iloc[sim_ind].values.tolist()
+    return {'juegos recomendados': list(sim_juegos)}
